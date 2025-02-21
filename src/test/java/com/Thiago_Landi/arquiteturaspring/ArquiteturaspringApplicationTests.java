eef898fc5f23185df5d3a226708afa7b92ab7612
Thiago-Landi <thiagolandi0902@gmail.com>
2025-02-21 12:43:23 -0300

    Project created

---
+++ src/test/java/com/Thiago_Landi/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Thiago_Landi.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
